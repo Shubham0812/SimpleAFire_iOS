46ef6f883efced40cd4566c777d8c22ea2d35914
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-17 22:39:22 +0530

    Modified ViewController

--- SimpleAFire/ViewController.swift
+++ SimpleAFire/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        APIManager.getResource(resourceID: 1) { (res) in
+//            print(res)
+//        }
+        
+//        APIManager.getAllResources { (results) in
+//            print(results, results.count)
+//        }
+        
+        let resource = Resource(id: 435, userId: 8, title: "Demo Resource", body: "A dummy resource for Medium Post")
+        
+        APIManager.postResource(res: resource)
     }
 
 
